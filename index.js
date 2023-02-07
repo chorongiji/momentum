865fe945354dce9c980ac0d3e2b141a1198895ad
chorongiji <124049756+chorongiji@users.noreply.github.com>
2023-02-07 18:31:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-$(document).ready(function () {
-    renderCurrentTime();
-    renderQuote();
-});
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+    <title>모멘텀앱</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
 
-function renderCurrentTime() {
-let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
-fetch(url)
-.then(res => res.json()).then((data) => {
-    let datetime = data['datetime'].substr(11,5);
-    $('#time').text(datetime);
-});
-}
+        * {
+            font-family: 'Poppins', sans-serif;
+        }
 
+        body {
+            background-image: url("youngnack01.JPG");
+        }        
+        
+    </style>
+    <script>
+        
+    </script>
+</head>
+<body>
+    <div class="main">
+				<div class="time">12:25</div>
+        <div class="greeting">Hello, Migdracios!</div>
+				<div class="todo">주 3회 운동하기</div>
+		</div>
+    </div>
+		<div class="quote">
+				<div class="author">- 명언 저자 - </div>
+				<div class="content">" 여기는 명언 내용이 들어갑니다! " </div>
+		</div>
+</body>
 
-function renderQuote() {
-    let url = `https://api.quotable.io/random`;
-    fetch(url)
-        .then(res => res.json()).then((data) => {
-            let content = `" ${data['content']} "`;
-            let author = `- ${data['author']} -`;
-            $('#content').text(content);
-            $('#author').text(author);
-        });
-}
+</html>
